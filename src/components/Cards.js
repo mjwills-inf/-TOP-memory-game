4c68377b697267407d7b277d2347759c04894e6e
mjwills-inf <mjwills@live.co.uk>
2020-11-13 21:04:32 +0000

    .

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,9 +3,13 @@ import React, { useState } from 'react'
 export default function Cards(props) {
   
   return (
-    <div className="card-div">
+    <div className="card-div" onClick={props.setClicked}>
       <div className="card-img">
-        <img className="image" src={`${props.src}`} alt={props.name}/>
+        <img 
+          className="image" 
+          src={`${props.src}`} 
+          alt={props.name}
+        />
       </div>
       <div className="card-title">        
         {props.name}
--- src/components/layout/MainContent.js
+++ src/components/layout/MainContent.js
@@ -3,17 +3,29 @@ import React from 'react'
 import Cards from '../Cards';
 
 import  { pictures } from '../data/data'
+let pictureData = pictures.map((item) => {
+  item.clicked = false
+  item.order = item.indexOf()
+  return item
+})
 
 export default function MainContent() {
 
-  let pictureData = pictures.map((item) => item)
+  const setClicked = (e) => {
+    console.log(e.target)
+    pictureData[e.target.props.id].clicked = true
+  }  
+
 
   return (
     <div className="main-content">
       <div className="grid-container">
         {pictureData.map((item) =>
            <Cards
-              key={item.id} 
+              clicked={item.clicked}
+              setClicked={setClicked}
+              key={item.id}
+              id={item.id} 
               name={item.name}
               src={item.src}
            />
